0325c3fea029e4ee00be8664aa06858e31373cd7
Adarsh M <adarshm954@gmail.com>
2019-01-08 22:18:01 +0530

    Add files via upload

---
+++ dist/list/addnew.php
@@ -0,0 +1,11 @@
+<?php
+	include('conn.php');
+ 
+	$list_title=$_POST['list_title'];
+	$list_location=$_POST['list_location'];
+	$list_description=$_POST['list_description'];
+	$query= "insert into list (list_title, list_location, list_description) values ('$list_title', '$list_location', '$list_description')";
+	mysqli_query($conn, $query);
+	header('location:index_list.php');
+ 
+?>
