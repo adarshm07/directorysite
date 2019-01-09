d17ec3264b7bee43d341b736d1875409de9c09c3
Adarsh M <adarshm954@gmail.com>
2019-01-09 10:27:24 +0530

    Update dist_.php

--- dist_.php
+++ dist_.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php
 require_once 'class/user.php';
 require_once 'conn.php';
@@ -14,21 +13,4 @@ if($_SESSION['user']['user_role'] == 2){
     header('Location: "C://wamp64/www/Thalassery-Official/login_.php"');
     exit();
 }
-=======
-<?php
-require_once 'class/user.php';
-require_once 'conn.php';
-session_start();
-if($_SESSION["user"] != true) {
-    echo("Access denied!");
-    header('Location: login_.php');
-    exit();
-}
-if($_SESSION['user']['user_role'] == 2){
-    header('Location: ./dist');
-} else {
-    header('Location: "C://wamp64/www/Thalassery-Official/login_.php"');
-    exit();
-}
->>>>>>> 8b0a5556a3d49d317d3da43a426807416db28832
 ?>
