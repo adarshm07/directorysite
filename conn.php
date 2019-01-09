66116d1c7fb4c7faf0d58b84dae4e4e01dc9ef1d
Adarsh M <adarshm954@gmail.com>
2019-01-09 10:16:17 +0530

    Updated

--- conn.php
+++ conn.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php
  
 //mysqli Procedural
@@ -7,14 +6,4 @@ if (!$conn) {
 	die("Connection failed: " . mysqli_connect_error());
 }
 
-=======
-<?php
- 
-//mysqli Procedural
-$conn = mysqli_connect("localhost","root","","sms");
-if (!$conn) {
-	die("Connection failed: " . mysqli_connect_error());
-}
-
->>>>>>> 8b0a5556a3d49d317d3da43a426807416db28832
 ?>
