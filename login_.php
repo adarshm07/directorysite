f3fb10a7fdada3f92165eaee51476d4c35483a78
Adarsh M <adarshm954@gmail.com>
2019-01-09 10:26:44 +0530

    Update login_.php

--- login_.php
+++ login_.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <html>
     <head>
         <title>Login</title>
@@ -12,19 +11,4 @@
 	$user->loginForm();
 ?>
     </body>
-=======
-<html>
-    <head>
-        <title>Login</title>
-    </head>
-    <body>
-    <?php
-	require_once 'class/user.php';
-	require_once 'config.php';
-	$user->indexHead();
-	$user->indexTop();
-	$user->loginForm();
-?>
-    </body>
->>>>>>> 8b0a5556a3d49d317d3da43a426807416db28832
 </html>
